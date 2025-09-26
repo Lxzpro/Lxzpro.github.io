c5cd959737c9c9922f6a19db3600cadd1875760a
Lxzpro <2287687348@qq.com>
2025-09-26 22:12:49 +0800

    Site updated: 2025-09-26 22:12:47

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["posts/16107.html","posts/20709.html","posts/49656.html"];function toRandomPost(){
+var posts=["posts/49656.html","posts/16107.html","posts/20709.html"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
