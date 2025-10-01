c15dfac39c4df879451766a13b3beb0515f5b7e2
Lxzpro <2287687348@qq.com>
2025-10-01 14:08:55 +0800

    Site updated: 2025-10-01 14:08:54

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["posts/10029.html","posts/3713.html","posts/16107.html","posts/20709.html","posts/14664.html","posts/49656.html"];function toRandomPost(){
+var posts=["posts/10029.html","posts/3713.html","posts/49656.html","posts/16107.html","posts/20709.html","posts/14664.html"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
